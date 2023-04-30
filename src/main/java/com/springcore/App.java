093c21f59b70278d6cbe6fad0e9d7fc5704d89f5
GG-Satya <dsatyaprasanna@gmail.com>
2023-04-30 12:03:13 +0530

    Shorten config file code with p-schema

--- src/main/java/com/springcore/App.java
+++ src/main/java/com/springcore/App.java
@@ -9,9 +9,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class App {
 	public static void main(String[] args) {
+		
 		System.out.println("Hello World!");
 		ApplicationContext context = new ClassPathXmlApplicationContext("Config.xml");
+		
 		Student student1 = (Student) context.getBean("student1");
+		Student student2 = (Student) context.getBean("student2");
+		
 		System.out.println(student1);
+		System.out.println(student2);
 	}
 }
