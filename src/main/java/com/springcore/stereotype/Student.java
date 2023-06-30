322ad82069e3b102e93896cfc995a3a120d4661f
GG-Satya <dsatyaprasanna@gmail.com>
2023-06-30 22:11:26 +0530

    Singleton vs Prototype Scope

--- src/main/java/com/springcore/stereotype/Student.java
+++ src/main/java/com/springcore/stereotype/Student.java
@@ -3,9 +3,11 @@ package com.springcore.stereotype;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("stud")
+@Scope("prototype")
 public class Student {
 	
 	@Value("gg satya")
--- src/main/java/com/springcore/stereotype/Test.java
+++ src/main/java/com/springcore/stereotype/Test.java
@@ -10,6 +10,15 @@ public class Test {
 		Student student = context.getBean("stud",Student.class);
 		System.out.println(student);
 		System.out.println(student.getAddresses().getClass().getName());
+		
+		System.out.println(student.hashCode());
+		
+		Student student2 = context.getBean("stud",Student.class);
+		System.out.println(student2.hashCode());
+		
+		Teacher t1 = context.getBean("teacher01",Teacher.class);
+		Teacher t2 = context.getBean("teacher01",Teacher.class);
+		System.out.println(t1.hashCode()+"--"+t2.hashCode());
 	}
 
 }
